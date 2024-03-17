5faa6e185fecdcfaabd57523a9ebf3ea09f10b7e
Rahul Biswakarma <62774412+rahul-biswakarma@users.noreply.github.com>
2024-03-17 18:25:00 +0530

    removed icon-generator

--- icons/icons.ts
+++
@@ -1,6 +0,0 @@
-export const ICON_TYPES = {
-  CHEVRON_LEFT:
-    "<svg viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M10 4L6 8L10 12' stroke='currentColor' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/></svg>",
-  HOLLOW_PLAY:
-    "<svg viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='M4.46289 11.3465V4.65343C4.46913 4.25606 4.68638 3.89203 5.0332 3.69781C5.38001 3.50359 5.80398 3.50853 6.14618 3.71076L11.9162 7.05839C12.2543 7.25142 12.4629 7.61077 12.4629 7.99998C12.4629 8.38918 12.2543 8.74853 11.9162 8.94157L6.14618 12.2892C5.80398 12.4914 5.38 12.4964 5.03319 12.3022C4.68638 12.1079 4.46913 11.7439 4.46289 11.3465V11.3465Z' stroke='currentColor' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/></svg>",
-};
