852101920851523458d760ce9c433fc675f2732a
Rahul Biswakarma <62774412+rahul-biswakarma@users.noreply.github.com>
2024-03-17 16:00:03 +0530

    added constants

---
+++ constants/prompt-enums.ts
@@ -0,0 +1,20 @@
+// Should be in sync with the prism schema
+export enum QuestionType {
+  ARRAY = "ARRAY",
+  SORTING = "SORTING",
+  PATH_FINDING = "PATH_FINDING",
+  GRAPH = "GRAPH",
+}
+
+export enum QuestionField {
+  BANKING = "BANKING",
+  GAME_DEVELOPMENT = "GAME_DEVELOPMENT",
+  BASIC = "BASIC",
+  MOSTLY_USED = "MOSTLY_USED",
+}
+
+export enum QuestionDifficulty {
+  EASY = "EASY",
+  MEDIUM = "MEDIUM",
+  HARD = "HARD",
+}
---
+++ constants/routes.ts
@@ -0,0 +1,11 @@
+export const PageRoutes = {
+  Dashboard: "/dashboard",
+  SignIn: "/sign-in",
+  SignUp: "/sign-up",
+  NewUser: "/new-user",
+};
+
+export const ApiRoutes = {
+  ProblemStatement: "/api/problem-statement",
+  User: "/api/user",
+};
